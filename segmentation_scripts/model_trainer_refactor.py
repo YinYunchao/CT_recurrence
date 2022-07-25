0d9d9855a42014b1000d370b46b46b135be37a1e
YinYunchao <44256873+YinYunchao@users.noreply.github.com>
2022-07-25 13:12:52 +0200

    Add files via upload
    the updated version added model saving funcs in train_manager, and it's the 1st version submitted to cluster

--- segmentation_scripts/model_trainer_refactor.py
+++ segmentation_scripts/model_trainer_refactor.py
@@ -1,3 +1,5 @@
+
+
 import torch
 from Unet_model import Vnet
 from image_loader import ImageLoadPipe
@@ -17,29 +19,33 @@ class TrainBuilder():
     taking two kwargs: one for read/write path, one for hyper parameters
     '''
     def __init__(self, paths, kwargs):
+        self.tf_path = paths['tfsummary']
+        self.result_savepath = paths['result']
+        self.model_SavePath = paths['model']
         self.dataloader = DataLoader(ImageLoadPipe(scan_path=paths['train_scan'],
-                                                   scan_list = os.listdir(paths['train_scan']),
+                                                   scan_list = os.listdir(paths['train_scan'])[0:1],
                                                    mask_path=paths['train_label'],
                                                    if_transform=True,
                                                    augmentation_list=['rotate','contrast',
                                                                       'gaussianNoise','GaussianBlur']),
                                      batch_size = 1, shuffle = True)
         self.val_dataloader = DataLoader(ImageLoadPipe(scan_path = paths['val_scan'],
-                                                    scan_list=os.listdir(paths['val_scan']),
+                                                    scan_list=os.listdir(paths['val_scan'])[0:1],
                                                     mask_path=paths['val_label'],
                                                     if_transform=False,
                                                     augmentation_list=None),
                                     batch_size = 1, shuffle=True)
+
         self.kwargs = kwargs
+        self.epoch = kwargs['epoch']
+        self.device = kwargs['device']
+        self.learning_rate = kwargs['lr']
+
         self.model = Vnet(elu=True, in_channel=1, classes=1)
         self.loss_func = DiceLoss()
-        self.writer = SummaryWriter(paths['tfsummary'])
-        self.device = kwargs['device']
         self.model.to(self.device)
-        self.TrainBuilder = TrainBuilder
-        self.epoch = kwargs['epoch']
-        self.learning_rate = kwargs['lr']
         self.optimizer = optim.SGD(self.model.parameters(), lr = self.learning_rate, momentum=0.9)
+
         self.TrainManager = TrainManager()
         
     
@@ -47,9 +53,9 @@ class TrainBuilder():
         '''
         the epoch loop, consist of training loop and validation loop
         '''
-        self.TrainManager.begin_run()
+        self.TrainManager.begin_run(self.kwargs,self.model,self.dataloader,self.tf_path)
         for e_ind in range(self.epoch):
-            running_loss = 0.0
+            self.TrainManager.begin_epoch()
             i=0
             for data in self.dataloader:
                 img, liver_mask = data
@@ -58,50 +64,50 @@ class TrainBuilder():
                 liver_mask = liver_mask.to(self.device, dtype = torch.float)
                 prediction = self.model(img)
                 loss = self.loss_func(prediction,liver_mask)
-                print(loss)
                 loss.backward()
                 self.optimizer.step()
-                running_loss += loss.item()
+
+                self.TrainManager.track_loss(loss, i)
                 i+=1
-                if i%10==0:
-                    # print(running_loss/i)
-                    self.writer.add_scalars("training loss",
-                                            {'training':running_loss},
-                                            i+e_ind*len(self.dataloader))
+
             self.model.train(False)
-            val_running_loss = 0.0
+            self.TrainManager.begin_validation()
             for val_data in self.val_dataloader:
                 val_img,val_mask = val_data
                 val_img = val_img.to(self.device)
                 val_mask = val_mask.to(self.device)
                 val_prediction = self.model(val_img)
                 val_loss = self.loss_func(val_prediction,val_mask)
-                val_running_loss += val_loss.detach()
-            print(val_running_loss)
-            self.writer.add_scalar("training loss",
-                                            {'validation':val_running_loss},
-                                            i+e_ind*len(self.dataloader))
-                
-        self.writer.flush()
+                self.TrainManager.track_val_loss(val_loss)
+            self.TrainManager.end_validation(len(self.val_dataloader))
+            self.TrainManager.save_best_model(self.model_SavePath)
+            self.TrainManager.end_epoch()
+        self.TrainManager.end_run()
+        self.TrainManager.save_result_csv(self.result_savepath,'train_result')
+
 
 
 path_cluster = {'train_scan':'/data/p288821/dataset/recurrence/training_scan/CT',
                 'train_label':'/data/p288821/dataset/recurrence/training_scan/livermask',
-                'tfsummary':'/data/p288821/tfsummary',
                 'val_scan':'/data/p288821/dataset/recurrence/validate_scan/CT',
-                'val_label':'/data/p288821/dataset/recurrence/validate_scan/livermask'}
+                'val_label':'/data/p288821/dataset/recurrence/validate_scan/livermask',
+                'tfsummary':'/data/p288821/tfsummary/recurrence',
+                'result': '/data/p288821/result/recurrence',
+                'model':'/data/p288821/result/recurrence'}
+
 path_local = {'train_scan':'X:/dataset/recurrence/training_scan/CT',
               'train_label':'X:/dataset/recurrence/training_scan/livermask',
-              'tfsummary':'c:/Users/yyc13/recurrence/tfsummary',
               'val_scan':'X:/dataset/recurrence/validate_scan/CT',
-              'val_label':'X:/dataset/recurrence/validate_scan/livermask'}
-params = {'device':'cpu',
-          'lr':0.001,
+              'val_label':'X:/dataset/recurrence/validate_scan/livermask',
+              'tfsummary':'c:/Users/yyc13/recurrence/tfsummary',
+              'result':'c:/Users/yyc13/recurrence/result',
+              'model':'c:/Users/yyc13/recurrence/result'}
+params = {'device':torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+          'lr':0.0001,
           'epoch':2}
 
 
-TrainBuilder(paths = path_local,kwargs=params)
-
+TrainBuilder(paths = path_cluster, kwargs=params).run()
 
 
         
--- segmentation_scripts/train_manager.py
+++ segmentation_scripts/train_manager.py
@@ -1,6 +1,9 @@
 import time
+import os
 from collections import OrderedDict
+from imageio import save
 import pandas as pd
+import torch
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
 
@@ -9,6 +12,11 @@ class TrainManager():
         self.epoch_count = 0
         self.epoch_loss = 0
         self.epoch_start_time = None
+
+        self.val_running_loss = 0
+        self.val_start_time = None
+        self.val_data = []
+        self.val_loss = []
 
         self.run_count = 0
         self.run_data = []
@@ -29,7 +37,7 @@ class TrainManager():
         run: the hyper parameter used in this training run
         network: the model
         loader: the loader
-        tfsummary_path: the folder(absolute path) to save tf record
+        tfsummary_path: the folder(absolute path) to save tf record 9
         '''
         self.run_start_time = time.time()
         self.run_params = run
@@ -45,6 +53,7 @@ class TrainManager():
         self.tf_writer.add_graph(self.network,img)
 
     def end_run(self):
+        self.tf_writer.flush()
         self.tf_writer.close()
         self.epoch_count = 0
         self.epoch_num_correct = 0
@@ -59,36 +68,77 @@ class TrainManager():
         run_duration = time.time()-self.run_start_time
 
         loss = self.epoch_loss/len(self.loader)
-        accuracy = self.epoch_num_correct / len(self.loader)
+        # accuracy = self.epoch_num_correct / len(self.loader)
         self.tf_writer.add_scalar('epoch_loss', loss, self.epoch_count)
-        self.tf_writer.add_scalar('epoch_acc', accuracy, self.epoch_count)
+        # self.tf_writer.add_scalar('epoch_acc', accuracy, self.epoch_count)
         if self.epoch_count%5==0:
             for name, param in self.network.named_parameters():
                 self.tf_writer.add_histogram(name, param, self.epoch_count)
                 self.tf_writer.add_histogram(f'{name}.grad',param.grad,self.epoch_count)
         results = OrderedDict()
-        results["run"] = self.run_count
+        # results["run"] = self.run_count
         results["epoch"] = self.epoch_count
         results["loss"] = loss
-        results["accuracy"] = accuracy
+        # results["accuracy"] = accuracy
         results["epoch_duration"] = epoch_duration
         results["run_duration"] = run_duration
-        for k,v in self.run_params._asdict().items():results[k] = v
+        for key in self.run_params: results[key] = self.run_params[key]
         self.run_data.append(results)
         # df = pd.DataFrame.from_dict(self.run_data,orient='columns')
 
-    def track_loss(self,loss,batch):
-        self.epoch_loss+=loss.item()*batch[0].shape[0]
+    def track_loss(self,loss,step):
+        self.epoch_loss+=loss.item()
+        self.tf_writer.add_scalar('step_loss', loss.item(), step+(self.epoch_count-1)*len(self.loader))
+
     def track_num_correct(self, prediction, label):
         self.epoch_num_correct+=self.get_num_correct(prediction, label)
 
     def _get_num_correct(self, prediction, labels):
+        '''
+        only used for classification tasks
+        '''
         return prediction.argmax(dim=1).eq(labels).sum().item()
-    def save(self, fileName):
 
+    def begin_validation(self):
+        self.val_running_loss = 0
+        self.val_start_time = time.time()
+
+    def track_val_loss(self, val_loss):
+        self.val_running_loss+=val_loss.item()
+        
+    def end_validation(self,val_dataNum):
+        val_results = OrderedDict()
+        val_results["epoch"] = self.epoch_count
+        val_results["loss"] = self.val_running_loss/val_dataNum
+        # val_results["accuracy"] = accuracy
+        val_results["epoch_duration"] = time.time()-self.val_start_time
+        #print('time for validation: ', val_results['epoch_duration'], ';',val_results['loss'])
+        self.val_running_loss = self.val_running_loss/val_dataNum
+        self.val_loss.append(val_results["loss"])
+        self.val_data.append(val_results)
+        self.tf_writer.add_scalar('epoch{}_validation_loss:'.format(self.epoch_count),
+                                    self.val_running_loss,self.epoch_count)
+        
+    def save_result_csv(self, save_path, fileName):
         pd.DataFrame.from_dict(
             self.run_data, orient='columns'
-        ).to_csv(f'{fileName}.csv')
+        ).to_csv(os.path.join(save_path,f'{fileName}.csv'))
+        pd.DataFrame.from_dict(
+            self.run_data, orient='columns'
+        ).to_csv(os.path.join(save_path,'val_{}.csv'.format(fileName)))
+
+    def save_best_model(self, save_path):
+        '''
+        The model will be saved if the validation loss is the smallest among all epochs
+        '''
+        if self.val_running_loss < min(self.val_loss):
+            torch.save(self.model.state_dict(),save_path)
+
+    def load_saved_model(self,load_path):
+        model = torch.load(load_path)
+        model.eval()
+
+
 
 
     
