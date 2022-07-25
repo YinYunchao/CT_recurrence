ada99097a9f85da0b04da37c0b69c7871c0e84b1
YinYunchao <44256873+YinYunchao@users.noreply.github.com>
2022-07-25 14:08:58 +0200

    Add files via upload
    correct a bug: in previous version, the img was not loaded into correct device in TrainManager for summary graph, fixed

--- segmentation_scripts/train_manager.py
+++ segmentation_scripts/train_manager.py
@@ -1,13 +1,21 @@
 import time
 import os
 from collections import OrderedDict
-from imageio import save
+from numpy import dtype
 import pandas as pd
 import torch
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
 
 class TrainManager():
+    '''
+    The class to refactor the training loop, functions including:
+        --count running time of each epoch and validation,
+        --generate tf summary
+        --summary the first image and label from loader, for chekcing img and mask
+        --summary the graph of model
+        --save best-performed model on validation set
+        '''
     def __init__(self):
         self.epoch_count = 0
         self.epoch_loss = 0
@@ -26,13 +34,9 @@ class TrainManager():
         self.loader = None
         self.tf_writer = None
     
-    def begin_run(self,run,network,loader,tfsummary_path):
+    def begin_run(self,run,network,loader,tfsummary_path,device):
         '''
-        Execute at the begining of training, including:
-        --count time,
-        --generate tf summary
-        --summary the first image and label from loader, for chekcing img and mask
-        --summary the graph of model
+        Execute at the begining of training;
         params:
         run: the hyper parameter used in this training run
         network: the model
@@ -47,23 +51,34 @@ class TrainManager():
         self.tf_writer = SummaryWriter(tfsummary_path)
 
         img,label = next(iter(self.loader))
+        img = img.to(device, dtype = torch.float)
+        label = label.to(device,dtype = torch.float)
         grid = torchvision.utils.make_grid([img[0,0,:,:,int(img.shape[-1]/2)],
                                             label[0,0,:,:,int(img.shape[-1]/2)]])
         self.tf_writer.add_image('img_check',grid)
         self.tf_writer.add_graph(self.network,img)
 
     def end_run(self):
+        '''
+        Execute at the end of training;
+        '''
         self.tf_writer.flush()
         self.tf_writer.close()
         self.epoch_count = 0
         self.epoch_num_correct = 0
     
     def begin_epoch(self):
+        '''
+        Execute at the begining of each epoch;
+        '''
         self.epoch_start_time = time.time()
         self.epoch_count+=1
         self.epoch_loss = 0
     
     def end_epoch(self):
+        '''
+        Execute at the end of each epoch;
+        '''
         epoch_duration= time.time()-self.epoch_start_time
         run_duration = time.time()-self.run_start_time
 
@@ -100,6 +115,9 @@ class TrainManager():
         return prediction.argmax(dim=1).eq(labels).sum().item()
 
     def begin_validation(self):
+        '''
+        Execute at the begining of validation;
+        '''
         self.val_running_loss = 0
         self.val_start_time = time.time()
 
@@ -107,6 +125,9 @@ class TrainManager():
         self.val_running_loss+=val_loss.item()
         
     def end_validation(self,val_dataNum):
+        '''
+        Execute at the end of validation;
+        '''
         val_results = OrderedDict()
         val_results["epoch"] = self.epoch_count
         val_results["loss"] = self.val_running_loss/val_dataNum
@@ -120,6 +141,9 @@ class TrainManager():
                                     self.val_running_loss,self.epoch_count)
         
     def save_result_csv(self, save_path, fileName):
+        '''
+        save the running loss, running time etc during the training
+        '''
         pd.DataFrame.from_dict(
             self.run_data, orient='columns'
         ).to_csv(os.path.join(save_path,f'{fileName}.csv'))
